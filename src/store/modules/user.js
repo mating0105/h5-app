a36e9d14e48d84bca11d8fc512c00afb3cb07cd6
mating <474200880@qq.com>
2019-12-11 16:30:24 +0800

    发起做单

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -27,7 +27,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
-        commit('SET_TOKEN', data.token)
+        commit('SET_TOKEN', data && data.token)
         setToken(data.token)
         resolve()
       }).catch(error => {
