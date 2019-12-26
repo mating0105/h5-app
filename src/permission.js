3b1adcdb9d5d5c51ab7cba4261cfb0f5b21f95b6
fengyi <1225160070@qq.com>
2019-12-26 19:19:41 +0800

    f4

--- src/permission.js
+++ src/permission.js
@@ -4,8 +4,8 @@ import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
 const loginForm = {
-  username: "gz0119",
-  password: "123"
+  username: "18349309486",
+  password: "123456"
 }
 
 
