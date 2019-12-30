4f3660cb08ff21f8be0c78a8c5c47fbbc9c26936
MRxiongyu <247770359@qq.com>
2019-12-30 20:56:31 +0800

    'fix'

--- src/views/gps/urlPage.vue
+++ src/views/gps/urlPage.vue
@@ -1,48 +1,51 @@
-
 <template>
-  <ViewPage>
-    <div class="xh-iframe-container">
-        <iframe
-                id="iframepage"
-                :src="url"
-                frameborder="0"
-                scrolling="auto"
-                width="100%"
-                :height="heightPage"
-        ></iframe>
-    </div>
-  </ViewPage>
+    <ViewPage>
+        <div :style="{height: heightPage + 'px'}" class="xh-iframe-container">
+            <iframe
+                    id="iframepage"
+                    :src="url"
+                    frameborder="0"
+                    scrolling="auto"
+            ></iframe>
+        </div>
+    </ViewPage>
 </template>
 <script>
-import Vue from "vue";
-import ViewPage from "@/layout/components/ViewPage";
-import { Toast } from "vant";
-Vue.use(Toast);
+  import Vue from "vue";
+  import ViewPage from "@/layout/components/ViewPage";
+  import { Toast } from "vant";
 
-export default {
-  components: {
-    ViewPage
-  },
-  data() {
-    return {
-      url: "",
-      heightPage: ""
-    };
-  },
-  mounted() {
-    this.url = this.$store.state.user.gpsUrl;
-    this.heightPage = document.body.clientHeight - 45;
-    window.addEventListener("message", e => {
-      console.log(e.data);
-      this.$router.go(-2);
-    },false);
-  }
-};
+  Vue.use(Toast);
+
+  export default {
+    components: {
+      ViewPage
+    },
+    data () {
+      return {
+        url: "",
+        heightPage: ""
+      };
+    },
+    mounted () {
+      this.url = this.$store.state.user.gpsUrl;
+      this.heightPage = document.body.clientHeight - 45;
+      window.addEventListener("message", e => {
+        console.log(e.data);
+        this.$router.go(-2);
+      }, false);
+    }
+  };
 </script>
 
 <style lang="scss">
     .xh-iframe-container {
         -webkit-overflow-scrolling: touch;
         overflow-y: scroll;
+    }
+
+    #iframepage {
+        width: 100%;
+        height: 100%;
     }
 </style>
