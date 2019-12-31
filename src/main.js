9d2ce285f38391c364037238a45d68f9c83856e8
wuyueqi <375953604@qq.com>
2019-12-31 20:03:25 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -15,8 +15,8 @@ Vue.mixin(mixins);
 
 import Bridge from './utils/JSbridge'
 Vue.prototype.$bridge = Bridge;
-Vue.prototype.$prefixurl = 'http://172.16.10.5:10004/#/';//测试gps
-// Vue.prototype.$prefixurl = 'http://xhh5.wwvas.com/#/';// 正式gps
+// Vue.prototype.$prefixurl = 'http://172.16.10.5:10004/#/';//测试gps
+Vue.prototype.$prefixurl = 'http://xhh5.wwvas.com/#/';// 正式gps
 // Vue.prototype.$prefixurl = 'http://192.168.50.55:8089/#/';//
 /**
  * If you don't want to use mock-server
