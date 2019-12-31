47c036a58764a16db994542a7820fb5ab371fd59
wuyueqi <375953604@qq.com>
2019-12-31 19:13:14 +0800

    修改

--- src/views/gps/index.vue
+++ src/views/gps/index.vue
@@ -7,7 +7,7 @@
     :scroll="true"
   >
     <template v-slot:head>
-      <van-search v-model="params.searchKey" :placeholder="pl" show-action @search="onSearch" />
+      <van-search v-model="params.searchKey" placeholder="请输入客户名称" show-action @search="onSearch" />
     </template>
     <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
       <van-list
@@ -101,8 +101,7 @@ export default {
         pageSize: 10
       },
       accout: "",
-      isLoading: false,
-      pl:'请输入客户信息'
+      isLoading: false
     };
   },
   computed: {
@@ -127,12 +126,6 @@ export default {
       this.loading = !this.isLoading;
       gpsList(this.params).then(res => {
         if (res.code == 200) {
-          if(this.pl == '请输入客户信息'){
-            this.pl = '111';
-          }else{
-            this.pl = '222';
-          }
-          
           setTimeout(() => {
             res.data.result.forEach(t => {
               switch (t.gpsIsDone) {
