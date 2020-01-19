41d8750d1fd715599e8cca48ee76241074eb495a
MRxiongyu <247770359@qq.com>
2020-01-19 21:42:27 +0800

    'fix'

--- src/views/makeLoan/lendProcess/index.vue
+++ src/views/makeLoan/lendProcess/index.vue
@@ -486,10 +486,10 @@
       goPage (val) {
         if (val.title === "GPS安装信息") {
           // if (!this.projectForm.gpsInfo) {
-          //   this.$notify({
-          //     type: "danger",
-          //     message: "未安装 GPS!"
-          //   });
+            this.$notify({
+              type: "danger",
+              message: "未安装 GPS!"
+            });
           //   return false;
           // } else {
           //   let url = `${this.$prefixurl}orderDetail?id=${this.projectForm.gpsInfo.orderId}&showTitle=false&externalid=${this.projectForm.projectInfo.projectNo}&externalcustnum=${this.projectForm.projectInfo.customNum}&externalvehicleid=${this.projectForm.projectInfo.cars[0].id}&username=${this.accout}&xhphonenum=${this.phone}&type=xh_h5`;
