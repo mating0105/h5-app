0298cf0cb8fd08202c3bda8eefb1151d4f2ce559
wuyueqi <375953604@qq.com>
2020-01-21 18:02:49 +0800

    修改bug

--- src/views/makeLoan/lendProcess/index.vue
+++ src/views/makeLoan/lendProcess/index.vue
@@ -494,8 +494,8 @@
             });
             return false;
           } else {
-            let url = `${this.$prefixurl}orderDetail?id=${gpsInfo.orderId}&showTitle=false&externalid=${this.projectForm.projectInfo.projectNo}&externalcustnum=${this.projectForm.projectInfo.customNum}&externalvehicleid=${this.projectForm.projectInfo.cars[0].id}&username=${this.accout}&xhphonenum=${this.phone}&type=xh_h5`;
-            //   //通知移动端加载gps安装页面
+            let url = `${this.$prefixurl}orderDetail?id=${gpsInfo.orderId}&showTitle=false&externalid=${this.projectForm.projectInfo.projectNo}&externalcustnum=${this.projectForm.projectInfo.customNum}&externalvehicleid=${this.projectForm.projectInfo.cars[0].id}&username=${this.projectForm.projectInfo.clientManager.name}&xhphonenum=${this.projectForm.projectInfo.clientManager.mobile}&type=xh_h5`;
+            //通知移动端加载gps安装页面
             Bridge.callHandler("loadUrl", url, data => {
               this.onLoad();
             });
