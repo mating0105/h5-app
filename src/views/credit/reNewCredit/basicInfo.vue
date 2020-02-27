6e99c1b155ecf78733d9e8f106dee2aa05a70e2a
MRxiongyu <247770359@qq.com>
2020-02-27 16:03:08 +0800

    'fix'

--- src/views/credit/reNewCredit/basicInfo.vue
+++ src/views/credit/reNewCredit/basicInfo.vue
@@ -111,7 +111,7 @@
 
     <Card style="margin-top: 1rem;">
       <template v-slot:header>
-        {{perInfoList.length === 0 ? '新增': ''}}征信客户
+        新增查询人
         <div class="card-icon" @click="addPer" v-if="edit">
           <van-icon name="add-o" />
         </div>
