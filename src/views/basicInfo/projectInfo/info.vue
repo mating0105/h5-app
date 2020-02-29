f1f32e2950014b17365eaae4fa0f0fff2adf364e
wuyueqi <375953604@qq.com>
2020-02-29 14:26:11 +0800

    xiugai

--- src/views/basicInfo/projectInfo/info.vue
+++ src/views/basicInfo/projectInfo/info.vue
@@ -855,6 +855,7 @@ export default {
       dataImg2: [], //评估报告
       message: "",
       userlist: false, //是否有下一节点人
+      peopleList:[],//选人数组
       processedBy: "",
       imageTypeList: ['6700', '6701', '6702', '6703', '6704', '6705', '6706', '6707', '6708', '6709', '6710', '6711', '6712', '6713', '6714', '6715', '6716', '6717', '6718', '6719', '6720', '6721', '6722', '6723'],//二手车照片类型
 
@@ -1176,7 +1177,12 @@ export default {
           this.show3 = true;
           break;
         case "下一节点处理人":
-          this.show3 = true;
+          this.options = this.peopleList;
+          console.log(this.options);
+          setTimeout(() =>{
+            this.show3 = true;
+          },1000)
+          
           break;
         default:
           break;
@@ -1920,9 +1926,12 @@ export default {
               label: t.companyName + "-" + t.name
             });
           });
-          this.options = objArr;
+          this.peopleList = objArr;
           this.loadType("下一节点处理人", "nextpeople");
-          this.dLoading = false;
+          setTimeout(()=>{
+            this.dLoading = false;
+          },1000)
+          
         })
         .catch(() => {
           this.dLoading = false;
